a80df3ae698a27b7b396609c8aa0ef6a670a525f
guillaume-c62 <67047151+guillaume-c62@users.noreply.github.com>
2021-01-21 14:03:03 +0100

    33
    35

--- frontend/facebouk/src/components/InscriptionPage/index.js
+++ frontend/facebouk/src/components/InscriptionPage/index.js
@@ -10,19 +10,20 @@ useEffect(()=>{fetchinsc(); fetchannul();})
     return(
         <div className={styles.container}>
             <div> 
-                <form>
-                    <label>Inscription</label>
+                <form className={styles.form}>
+                    <label className={styles.Name} >Inscription</label>
+                    <div className={styles.login}>
                         <input className={styles.inputInscription} id="username" type="text" method="post" for="" placeholder="Identifiant ..."></input>
                         <input className={styles.inputInscription} id="email" type="text" method="post" for="email" placeholder="Adresse mail ..."></input>
                         <input className={styles.inputInscription} id="password" type="password" method="post" for="password" placeholder="Mot de passe ..."></input>
-                        <input className={styles.inputInscription} type="password" method="post" for="password" placeholder="Confirmez le mot de passe ..."></input>     
-                        <div> 
-                        <button id="cancel">Annuler</button>
-                        </div>             
-                        <div>
-                            <button id="submit" type="submit">Je m'inscris</button>
-                        </div>
+                        <input className={styles.inputInscription} type="password" method="post" for="password" placeholder="Confirmez le mot de passe ..."></input>  
+                    </div>            
                 </form>
+                <div className={styles.btn}> 
+                        <button className={styles.log} id="submit" type="submit">Je m'inscris</button>
+                            <button className={styles.dead} id="cancel">Annuler</button>
+                            
+                        </div> 
                 <p id="error"></p>
             </div>
         </div>
